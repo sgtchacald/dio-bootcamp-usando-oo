d72866f214624c9b8b6a364a6e3fbaeeeec5f427
ddscordeiro <sgt.chacal.d@gmail.com>
2023-11-21 17:57:44 -0300

    Refatoração

--- src/dominio/Bootcamp.java
+++ src/dominio/Bootcamp.java
@@ -1,4 +1,4 @@
-package br.com.dio.desafio.dominio;
+package src.dominio;
 
 import java.time.LocalDate;
 
---
+++ src/dominio/Curso.java
@@ -0,0 +1,27 @@
+package src.dominio;
+
+public class Curso extends Conteudo{
+    private Integer cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
