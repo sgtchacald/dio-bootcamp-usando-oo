8409fe522d4fa4bbdd213069071b56813a47de26
ddscordeiro <sgt.chacal.d@gmail.com>
2023-11-21 17:38:25 -0300

    Adicionando Classe Conteúdo para implementar a herança nas classes Curso e Menoria

--- br/com/dio/desafio/dominio/Mentoria.java
+++ br/com/dio/desafio/dominio/Mentoria.java
@@ -2,32 +2,8 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
     private LocalDate dataRealizacao;
-
-    public Mentoria(String titulo, String descricao, LocalDate dataRealizacao) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.dataRealizacao = dataRealizacao;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getDataRealizacao() {
         return dataRealizacao;
@@ -40,9 +16,14 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", dataRealizacao=" + dataRealizacao +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + dataRealizacao +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
 }
